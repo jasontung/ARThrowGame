e10963abb40d19e9df526ac51adf89fd71806923
jasontung2214 <jasontung2214@gmail.com>
2018-05-29 22:07:26 +0800

    no message

---
+++ Assets/Scripts/DistanceChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DistanceChecker : MonoBehaviour {
+    public Transform target;
+    public float distance;
+    public Color wrongColor;
+    public Color rightColor;
+    public Slider distanceIndicator;
+    public Image indicatorImage;
+    public ThrowController throwController;
+	void LateUpdate ()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+        distanceIndicator.value = distance;
+        indicatorImage.color = Color.Lerp(wrongColor, rightColor, distanceIndicator.normalizedValue);
+        throwController.enabled = distanceIndicator.value == distanceIndicator.maxValue;
+	}
+}
---
+++ Assets/Scripts/ThrowController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+public class ThrowController : MonoBehaviour {
+    public Slider powerIndicator;
+    private bool isThrew;
+    public float maxPower;
+    public Rigidbody ball;
+    public Transform throwPos;
+    public Button restartButton;
+    private void Awake()
+    {
+        restartButton.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (isThrew)
+            return;
+		if(Input.GetMouseButton(0))
+        {
+            powerIndicator.value += Time.deltaTime;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Throw();
+        }
+	}
+
+    public void Throw()
+    {
+        isThrew = true;
+        ball.transform.SetParent(null);
+        ball.isKinematic = false;
+        ball.AddForce(throwPos.forward * maxPower * powerIndicator.normalizedValue);
+        Invoke("DelayShowRestartButton", 3f);
+    }
+
+    private void DelayShowRestartButton()
+    {
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
